27d55d831f2c4adfd65bde8878155b27da20dac0
Rodrigo Amorim <rodrigoamorimdhy@gmail.com>
2021-09-23 09:39:27 -0300

    poo inicio

---
+++ funcao/gerador_html_v5.py
@@ -0,0 +1,27 @@
+bloco_atrs = ('bloco_accesskey', 'bloco_id')
+ul_atrs = ('ul_id', 'ul_style')
+
+def get_atrs(informados, suportados):
+    return ' '.join(f'{k.split("_")[-1]} = "{v}"' for k, v in informados.items() if k in suportados)
+
+def tag_bloco(conteudo, *args, classe='success', inline=False, **novos_atrs):
+    tag = 'span' if inline else 'div'
+    html = conteudo if not callable(conteudo) else conteudo(*args, **novos_atrs)
+    atributos = get_atrs(novos_atrs, bloco_atrs)
+    return f'<{tag} {atributos} class="{classe}">{html}</{tag}>'
+
+def tac_lista(*itens, **novos_atrs):
+    lista = ''.join(f'<li>{item}</li>' for item in itens)
+    return f'<ul {get_atrs(novos_atrs, ul_atrs)}>{lista}</ul>'
+
+if __name__ == '__main__':
+
+    print(tag_bloco('bloco'))
+    print(tag_bloco('inline e classe', 'info', True))
+    print(tag_bloco('inline', inline=True))
+    print(tag_bloco(inline=True, conteudo='inline'))
+    print(tag_bloco('flahou', inline='error'))
+    print(tag_bloco(tac_lista('Item 1', 'Item 2'), classe='info'))
+    print(tag_bloco(tac_lista('Sábado', 'Domingo'), classe='info', inline=True))
+
+    print(tag_bloco(tac_lista, 'Item 1', 'Item 2', classe='info', bloco_accesskey='m', bloco_id='conteudo'))
